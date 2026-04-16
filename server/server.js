6cfc3b1ba1eada69c96437638816c3b60bfeef10
casualweb205-boop <casualweb205@gmail.com>
2026-04-16 17:52:17 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -36,7 +36,9 @@ app.get("/api", (req, res) => {
 });
 
 // Server
-const PORT = 5000;
+
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
